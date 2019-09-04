16c857271128c04f0fe28188a21eaa617c488ac4
Maickel <maickelpc@gmail.com>
2019-09-04 20:07:08 -0300

    Index

--- skalla/empresa/admin.py
+++ skalla/empresa/admin.py
@@ -2,17 +2,17 @@ from django_reverse_admin import ReverseModelAdmin
 from pessoa.admin import PessoaAdminInline
 from django.contrib import admin
 
-from .forms import EmpresaForm
+# from .forms import EmpresaForm
 from .models import  Empresa
 
 # Register your models here.
 
 # class EmpresaAdmin(ReverseModelAdmin):
-class EmpresaAdmin(admin.ModelAdmin):
+class EmpresaAdmin(ReverseModelAdmin):
     search_fields = ['empresa.nome','nomeFantasia']
-    # inline_reverse = [('pessoa', {'inlines': ['endereco']})]
-    # inline_type = 'stacked'
-    form = EmpresaForm
+    inline_reverse = [('pessoa', {'inlines': ['endereco']})]
+    inline_type = 'stacked'
+    # form = EmpresaForm
     # inlines = []
     # list_display = ['id', 'empresa.nome','nomeFantasia']
     # list_display_links = ['id', 'empresa.nome','nomeFantasia']
