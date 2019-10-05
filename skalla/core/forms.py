d46fac4c74569a2ef763c9c86cdc9d0b64844bc4
Maickel <maickelpc@gmail.com>
2019-10-05 10:21:07 -0300

    Ajuste para Migration

--- skalla/core/forms.py
+++ skalla/core/forms.py
@@ -1,19 +1,19 @@
 from django import forms
-from empresa.models import Empresa
-from cliente.models import Cliente, Turno, PontoAlocacao
-
-class Formulario(forms.ModelForm):
-    campoqua = forms.CharField(max_length=100, required=True, label="Nome")
-
-    class Meta:
-        model = Empresa
-        fields = '__all__'
-
-
-class FormInicialEscala(forms.Form):
-    cliente = forms.ChoiceField(label='Cliente:', choices=[('0', '-- Selecione --')] +
-                                        [(cliente.id, cliente.nome) for cliente in Cliente.objects.all()])
-    turno = forms.ChoiceField(label='Turno', choices=[('0', '-- Selecione --')] +
-                                        [(turno.id, turno.descricao) for turno in Turno.objects.all()])
-    ponto = forms.ChoiceField(choices=[('0', '-- Selecione --')] +
-                                        [(ponto.id, ponto.nome) for ponto in PontoAlocacao.objects.all()])
+# from empresa.models import Empresa
+# from cliente.models import Cliente, Turno, PontoAlocacao
+#
+# class Formulario(forms.ModelForm):
+#     campoqua = forms.CharField(max_length=100, required=True, label="Nome")
+#
+#     class Meta:
+#         model = Empresa
+#         fields = '__all__'
+#
+#
+# class FormInicialEscala(forms.Form):
+#     cliente = forms.ChoiceField(label='Cliente:', choices=[('0', '-- Selecione --')] +
+#                                         [(cliente.id, cliente.nome) for cliente in Cliente.objects.all()])
+#     turno = forms.ChoiceField(label='Turno', choices=[('0', '-- Selecione --')] +
+#                                         [(turno.id, turno.descricao) for turno in Turno.objects.all()])
+#     ponto = forms.ChoiceField(choices=[('0', '-- Selecione --')] +
+#                                         [(ponto.id, ponto.nome) for ponto in PontoAlocacao.objects.all()])
--- skalla/core/views.py
+++ skalla/core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 # from django.template import Context, Template
 from django.http import HttpResponse
 from django.shortcuts import render
-from .forms import Formulario
+# from .forms import Formulario
 from empresa.models import Empresa
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import FormMixin
@@ -12,15 +12,16 @@ from django.views.generic import ListView, TemplateView
 from django.core.mail import send_mail
 from django import forms
 from cliente.models import Cliente, Escala
-from .forms import FormInicialEscala
+# from .forms import FormInicialEscala
 
 # Create your views here.
 
 
 def index(request):
-    contexto = {
-        'meuform': FormInicialEscala(),
-        'escala' : Escala()
-    }
-    return render(request, 'criaEscala.html', contexto)
+    # contexto = {
+    #     'meuform': FormInicialEscala(),
+    #     'escala' : Escala()
+    # }
+    # return render(request, 'criaEscala.html', contexto)
+    return render(request, 'criaEscala.html')
 
